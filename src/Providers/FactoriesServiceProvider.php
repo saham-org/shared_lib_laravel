da9c5ee41b2abf9e87fd4bb9f9e2760d902e794e
Ahmed A <you@example.com>
2023-04-03 21:48:27 +0200

    guess correct factory names

--- src/Providers/FactoriesServiceProvider.php
+++ src/Providers/FactoriesServiceProvider.php
@@ -4,13 +4,22 @@ namespace Saham\SharedLibs\Providers;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class FactoriesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // Factory::guessFactoryNamesUsing(function (string $modelName) {
-        //     return 'Saham\\SharedLibs\\Database\\Factories\\'.class_basename($modelName).'Factory';
-        // });
+        Factory::guessFactoryNamesUsing(function (string $modelName) {
+            // We can also customise where our factories live too if we want:
+            $namespace = 'Saham\\SharedLibs\\Database\\Factories\\';
+
+            // Here we are getting the model name from the class namespace
+            $modelName = Str::afterLast($modelName, '\\');
+
+            // Finally we'll build up the full class path where
+            // Laravel will find our model factory
+            return $namespace.$modelName.'Factory';
+        });
     }
 }
