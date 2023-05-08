37d39178f725bf8bf58b443bda360a84f815cb3d
Ahmed A <you@example.com>
2023-05-08 17:10:13 +0200

    feast details

--- src/Models/Feast.php
+++ src/Models/Feast.php
@@ -3,10 +3,16 @@
 namespace Saham\SharedLibs\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Saham\SharedLibs\Mongodb\Relations\HasMany;
 
 class Feast extends Order
 {
     use HasFactory;
 
     protected $table = 'feasts';
+
+    public function details(): HasMany
+    {
+        return $this->hasMany(FeastDetails::class);
+    }
 }
