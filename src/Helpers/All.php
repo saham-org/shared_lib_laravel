ff09ea8d94d8594fb3b2714c3ac7bf953226feeb
Ahmed A <you@example.com>
2023-05-28 20:11:44 +0200

    store accepts array

--- src/Helpers/All.php
+++ src/Helpers/All.php
@@ -1,59 +1,65 @@
 <?php
 
+use Saham\SharedLibs\Models\Store;
 
 if (!function_exists('getStoreServices')) {
-    function getStoreServices($store, $default = false): mixed
+    /**
+     * @param Store $store,
+     * @param bool $default
+     * @return array<string, boolean>
+     */
+    function getStoreServices(Store $store, $default = false): mixed
     {
         return isset($store->services) ? getStoreServicesFromStore($store) : getStoreServicesFromNull($default);
     }
 }
 
-
 if (!function_exists('getStoreServicesFromStore')) {
-    function getStoreServicesFromStore($store): mixed
+    function getStoreServicesFromStore(Store $store): mixed
     {
         return [
-            'delivery' => $store->services['delivery'] ?? false ,
-            'pickup' => $store->services['pickup'] ?? false ,
-            'reservation' => $store->services['reservation'] ?? false ,
-            'feasts' => $store->services['feasts'] ?? false ,
+            'delivery' => $store->services['delivery'] ?? false,
+            'pickup' => $store->services['pickup'] ?? false,
+            'reservation' => $store->services['reservation'] ?? false,
+            'feasts' => $store->services['feasts'] ?? false,
         ];
     }
 }
 
 if (!function_exists('getStoreServicesFromNull')) {
+    /**
+     * @param bool $default
+     * @return array<string, boolean>
+     */
     function getStoreServicesFromNull($default = false): mixed
     {
         return [
             'delivery' => $default,
             'pickup' => $default,
             'reservation' => $default,
-            'feasts' => $default ,
+            'feasts' => $default,
         ];
     }
 }
 
-
-
-
 if (!function_exists('getDistanceInMeter')) {
     /**
-    * Calculates the great-circle distance between two points, with
-    * the Vincenty formula.
-    * @param float $latitudeFrom Latitude of start point in [deg decimal]
-    * @param float $longitudeFrom Longitude of start point in [deg decimal]
-    * @param float $latitudeTo Latitude of target point in [deg decimal]
-    * @param float $longitudeTo Longitude of target point in [deg decimal]
-    * @param float $earthRadius Mean earth radius in [m]
-    * @return float Distance between points in [m] (same as earthRadius)
-    */
+     * Calculates the great-circle distance between two points, with
+     * the Vincent formula.
+     * @param float $latitudeFrom Latitude of start point in [deg decimal]
+     * @param float $longitudeFrom Longitude of start point in [deg decimal]
+     * @param float $latitudeTo Latitude of target point in [deg decimal]
+     * @param float $longitudeTo Longitude of target point in [deg decimal]
+     * @param float $earthRadius Mean earth radius in [m]
+     * @return float Distance between points in [m] (same as earthRadius)
+     */
     function getDistanceInMeter(
         $latitudeFrom,
         $longitudeFrom,
         $latitudeTo,
         $longitudeTo,
         $earthRadius = 6371000
-    ) {
+    ): float {
         // convert from degrees to radians
         $latFrom = deg2rad($latitudeFrom);
         $lonFrom = deg2rad($longitudeFrom);
@@ -62,10 +68,47 @@ if (!function_exists('getDistanceInMeter')) {
 
         $lonDelta = $lonTo - $lonFrom;
         $a = pow(cos($latTo) * sin($lonDelta), 2) +
-          pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
+            pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
         $b = sin($latFrom) * sin($latTo) + cos($latFrom) * cos($latTo) * cos($lonDelta);
 
         $angle = atan2(sqrt($a), $b);
         return $angle * $earthRadius;
     }
+}
+
+
+/**
+ * @param Store $store
+ * @param bool $default
+ * @return array<string, boolean>
+ */
+function getStoreAcceptArray(Store $store, $default = true): mixed
+{
+    return isset($store->accepts) ? getStoreAcceptFromStore($store, $default) : getStoreAcceptFromNull($default);
+}
+/**
+ * @param Store $store
+ * @param boolean $default
+ * @return array<string, boolean>
+ */
+function getStoreAcceptFromStore(Store $store, bool $default = true): mixed
+{
+    return [
+        'online'    => $store->accepts['online'] ?? $default,
+        'wallet'    => $store->accepts['wallet'] ?? $default,
+        'cash'      => $store->accepts['cash'] ?? $default,
+    ];
+}
+
+/**
+ * @param boolean $default
+ * @return array<string, boolean>
+ */
+function getStoreAcceptFromNull($default = true): mixed
+{
+    return [
+        'online'    => $default,
+        'wallet'    => $default,
+        'cash'      => $default,
+    ];
 }
