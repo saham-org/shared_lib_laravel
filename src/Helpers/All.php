8d2aa8f30d396966fce38ff5e6243cfdd48e6668
Ahmed A <you@example.com>
2023-05-28 22:15:24 +0200

    store accepts array

--- src/Helpers/All.php
+++ src/Helpers/All.php
@@ -1,13 +1,12 @@
 <?php
 
-use Saham\SharedLibs\Database\Factories\StoreFactory;
 use Saham\SharedLibs\Models\Store;
-use Saham\SharedLibs\Mongodb\Eloquent\Model;
 
 if (!function_exists('getStoreServices')) {
     /**
      * @param Store $store,
-     * @param bool $default
+     * @param bool  $default
+     *
      * @return array<string, boolean>
      */
     function getStoreServices(object $store, $default = false): mixed
@@ -20,10 +19,10 @@ if (!function_exists('getStoreServicesFromStore')) {
     function getStoreServicesFromStore(object $store): mixed
     {
         return [
-            'delivery' => $store->services['delivery'] ?? false,
-            'pickup' => $store->services['pickup'] ?? false,
+            'delivery'    => $store->services['delivery'] ?? false,
+            'pickup'      => $store->services['pickup'] ?? false,
             'reservation' => $store->services['reservation'] ?? false,
-            'feasts' => $store->services['feasts'] ?? false,
+            'feasts'      => $store->services['feasts'] ?? false,
         ];
     }
 }
@@ -31,15 +30,16 @@ if (!function_exists('getStoreServicesFromStore')) {
 if (!function_exists('getStoreServicesFromNull')) {
     /**
      * @param bool $default
+     *
      * @return array<string, boolean>
      */
     function getStoreServicesFromNull($default = false): mixed
     {
         return [
-            'delivery' => $default,
-            'pickup' => $default,
+            'delivery'    => $default,
+            'pickup'      => $default,
             'reservation' => $default,
-            'feasts' => $default,
+            'feasts'      => $default,
         ];
     }
 }
@@ -47,11 +47,13 @@ if (!function_exists('getStoreServicesFromNull')) {
 /**
  * Calculates the great-circle distance between two points, with
  * the Vincent formula.
- * @param float $latitudeFrom Latitude of start point in [deg decimal]
+ *
+ * @param float $latitudeFrom  Latitude of start point in [deg decimal]
  * @param float $longitudeFrom Longitude of start point in [deg decimal]
- * @param float $latitudeTo Latitude of target point in [deg decimal]
- * @param float $longitudeTo Longitude of target point in [deg decimal]
- * @param float $earthRadius Mean earth radius in [m]
+ * @param float $latitudeTo    Latitude of target point in [deg decimal]
+ * @param float $longitudeTo   Longitude of target point in [deg decimal]
+ * @param float $earthRadius   Mean earth radius in [m]
+ *
  * @return float Distance between points in [m] (same as earthRadius)
  */
 function getDistanceInMeter(
@@ -64,26 +66,31 @@ function getDistanceInMeter(
     // convert from degrees to radians
     $latFrom = deg2rad($latitudeFrom);
     $lonFrom = deg2rad($longitudeFrom);
-    $latTo = deg2rad($latitudeTo);
-    $lonTo = deg2rad($longitudeTo);
+    $latTo   = deg2rad($latitudeTo);
+    $lonTo   = deg2rad($longitudeTo);
 
     $lonDelta = $lonTo - $lonFrom;
-    $a = pow(cos($latTo) * sin($lonDelta), 2) +
+    $a        = pow(cos($latTo) * sin($lonDelta), 2) +
         pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
     $b = sin($latFrom) * sin($latTo) + cos($latFrom) * cos($latTo) * cos($lonDelta);
 
     $angle = atan2(sqrt($a), $b);
+
     return $angle * $earthRadius;
 }
 
-
 /**
  * @param object $object
- * @param bool $default
+ * @param bool   $default
+ *
  * @return array<string, boolean>
  */
 function getSystemPaymentMethods(object $object, $default = true): mixed
 {
+    if (!$object || !property_exists($object, 'getPaymentTypesAttribute')) {
+        return getSystemPaymentMethodsFromNull($default);
+    }
+
     return [
         'online'    => $object->getPaymentTypesAttribute()['online'] ?? $default,
         'wallet'    => $object->getPaymentTypesAttribute()['wallet'] ?? $default,
@@ -91,9 +98,9 @@ function getSystemPaymentMethods(object $object, $default = true): mixed
     ];
 }
 
-
 /**
- * @param boolean $default
+ * @param bool $default
+ *
  * @return array<string, boolean>
  */
 function getSystemPaymentMethodsFromNull($default = true): mixed
