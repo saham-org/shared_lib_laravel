d84509cebc8512c3ef17f9f44131d2e62d150d82
amryami1 <amr.yami1@gmail.com>
2023-06-22 11:22:43 +0200

    unit model

---
+++ src/Models/Unit.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Saham\SharedLibs\Models;
+
+use Saham\SharedLibs\Models\Abstracts\BaseModel;
+use Saham\SharedLibs\Mongodb\Eloquent\SoftDeletes;
+use Saham\SharedLibs\Traits\Translatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Unit extends BaseModel
+{
+    use HasFactory;
+    use Translatable;
+    use SoftDeletes ;
+
+    protected $fillable = [
+        'number', 'adult_min', 'adult_max', 'child_min', 'child_max', 'adult_price', 'child_price', 'imgs'
+    ];
+
+
+}
