61ec4f84510b48ff71970ff358c278efabfd8a3a
amryami1 <amr.yami1@gmail.com>
2023-07-04 12:58:40 +0200

    remover connection from activity

--- src/Models/Activity.php
+++ src/Models/Activity.php
@@ -6,7 +6,7 @@ use Saham\SharedLibs\Mongodb\Eloquent\Model;
 
 class Activity extends Model
 {
-    protected $connection = 'mongodb_log';
+//    protected $connection = 'mongodb_log';
     protected $table      = 'activity_log';
     protected $fillable   = ['event', 'related_id', 'related_type', 'guard_name', 'subject', 'causer', 'properties', 'old_properties', 'date'];
 }
