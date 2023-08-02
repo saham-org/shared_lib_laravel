4628fc120cde3a962fd57e615af58597a06b4b6e
ahmedashraf093 <118922563+ahmedashraf093@users.noreply.github.com>
2023-08-02 13:15:10 +0300

    add interaction model

---
+++ src/Models/Interaction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Saham\SharedLibs\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Saham\SharedLibs\Models\Abstracts\BaseModel;
+
+class Interaction extends BaseModel
+{
+    use HasFactory;
+
+}
