290e9caffa6d0f5294d53e7719b15930f83c55cf
abdelaziz <abdelaziz.nasser24@gmail.com>
2023-11-15 13:39:54 +0200

    Add state machine for feasts

--- src/Models/Feast.php
+++ src/Models/Feast.php
@@ -5,11 +5,27 @@ namespace Saham\SharedLibs\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Saham\SharedLibs\Mongodb\Relations\BelongsTo;
 use Saham\SharedLibs\Mongodb\Relations\HasMany;
+use Saham\SharedLibs\StateMachines\FeastStatusMachine;
+use Saham\SharedLibs\Traits\HasNotes;
+use Saham\SharedLibs\Traits\HasStateMachines;
+use Saham\SharedLibs\Models\Abstracts\BaseModel;
+use Saham\SharedLibs\Models\Enums\FeastStatus;
+use Saham\SharedLibs\Mongodb\Eloquent\Builder;
 
 class Feast extends Order
 {
     use HasFactory;
+    use HasNotes;
+    use HasStateMachines;
 
+    /**
+     * `status` State Machines
+     * @var array
+     */
+    public $stateMachines = [
+        'status' => FeastStatusMachine::class
+    ];
+    protected $guarded = [];
     protected $table = 'feasts';
 
     public function details(): HasMany
@@ -30,5 +46,25 @@ class Feast extends Order
     public function partner(): BelongsTo
     {
         return $this->belongsTo(Partner::class);
+    }
+
+    public function complaints(): HasMany
+    {
+        return $this->hasMany(Complaint::class, 'feast_id');
+    }
+
+    public function ratings(): HasMany
+    {
+        return $this->hasMany(Rating::class, 'feast_id');
+    }
+
+    public function complains(): HasMany
+    {
+        return $this->hasMany(Complaint::class);
+    }
+
+    public function userFeastTransaction(): HasMany
+    {
+        return $this->hasMany(UserTransaction::class, 'feast_id');
     }
 }
---
+++ src/StateMachines/FeastStatusMachine.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Saham\SharedLibs\StateMachines;
+
+use Saham\SharedLibs\Models\Enums\FeastStatus;
+
+class FeastStatusMachine extends BaseStateMachine
+{
+    public function recordHistory(): bool
+    {
+        return true;
+    }
+
+    /**
+     * set default status of newly created models
+     */
+    public function defaultState(): ?string
+    {
+        return FeastStatus::Pending->value;
+    }
+
+    /**
+     * define list of available transitions
+     * @return array<string,array<string,callable>>
+     */
+    public function transitions(): array
+    {
+        return [
+            // from => to pattern
+            '*' => [
+                FeastStatus::Pending->value => static fn ($model, $who) => true,
+            ],
+
+            // handle pending transitions
+            FeastStatus::Pending->value => [
+                FeastStatus::Approved->value => fn ($model, $who) => $this->inGroup(['managers', 'administrators', 'partners'], $who),
+                FeastStatus::Rejected->value => fn ($model, $who) => $this->inGroup(['managers', 'administrators', 'partners'], $who),
+
+                FeastStatus::Expired->value => fn ($model, $who) => $this->inGroup([], $who),
+                FeastStatus::Cancelled->value => fn ($model, $who) => $this->inGroup(['users', 'administrators'], $who),
+            ],
+
+            // handle approved transitions
+            FeastStatus::Approved->value => [
+                FeastStatus::Rejected->value => fn ($model, $who) => $this->inGroup(['managers', 'administrators', 'partners'], $who),
+                FeastStatus::Cancelled->value => fn ($model, $who) => $this->inGroup(['users', 'administrators'], $who),
+                FeastStatus::Payed->value => fn ($model, $who) => $this->inGroup(['users'], $who),
+            ],
+
+            // handle payed transitions
+            FeastStatus::Payed->value => [
+                FeastStatus::Preparing->value => fn ($model, $who) => $this->inGroup(['managers', 'administrators', 'partners'], $who),
+
+            ],
+
+
+
+            // handle preparing transitions
+            FeastStatus::Preparing->value => [
+                FeastStatus::Prepared->value => fn ($model, $who) => $this->inGroup(['managers', 'administrators', 'partners'], $who),
+
+            ],
+
+            // handle prepared transitions
+            FeastStatus::Prepared->value => [
+                FeastStatus::InLocation->value => fn ($model, $who) => $this->inGroup(['managers', 'administrators', 'partners'], $who) && $model->deliver_type === 'receipt',
+
+
+                FeastStatus::Preparing->value => fn ($model, $who) => $this->inGroup(['managers', 'administrators', 'partners'], $who),
+
+            ],
+
+
+            // handle in location transitions
+            FeastStatus::InLocation->value => [
+                FeastStatus::Prepared->value => fn ($model, $who) => $this->inGroup(['managers', 'administrators', 'partners'], $who),
+
+                FeastStatus::Completed->value => fn ($model, $who) => $this->inGroup(['managers', 'administrators', 'partners'], $who) && $model->deliver_type === 'delivery',
+
+             ],
+
+            // handle rejected transitions
+            FeastStatus::Rejected->value => [
+                FeastStatus::Cancelled->value => fn ($model, $who) => $this->inGroup([], $who),
+                FeastStatus::Refunded->value => fn ($model, $who) => $this->inGroup([], $who),
+                FeastStatus::Pending->value => fn ($model, $who) => $this->inGroup(['administrators'], $who),
+            ],
+
+            // handle expired transitions
+            FeastStatus::Expired->value => [
+                FeastStatus::Cancelled->value => fn ($model, $who) => $this->inGroup([], $who),
+                FeastStatus::Pending->value => fn ($model, $who) => $this->inGroup(['administrators', 'users'], $who),
+                FeastStatus::Refunded->value => fn ($model, $who) => $this->inGroup(['administrators', 'users'], $who),
+            ],
+
+            // handle cancelled transitions
+            FeastStatus::Cancelled->value => [
+                FeastStatus::Refunded->value => fn ($model, $who) => $this->inGroup([], $who),
+            ],
+
+            // handle refunded transitions
+            FeastStatus::Refunded->value => [],
+
+            // handle completed transitions
+            FeastStatus::Completed->value => [],
+
+        ];
+    }
+}
