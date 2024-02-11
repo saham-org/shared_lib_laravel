1fd19055b61855fcac333aa55bfbfdbb7fcd1f12
abdelaziz <abdelaziz.nasser24@gmail.com>
2024-02-11 15:12:09 +0200

    updates in model user and adding wallet

---
+++ src/Models/Wallet.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Saham\SharedLibs\Models;
+
+use DateTime;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use MongoDB\BSON\UTCDateTime;
+use Saham\SharedLibs\Models\Abstracts\BaseModel;
+use Saham\SharedLibs\Mongodb\Eloquent\SoftDeletes;
+use Saham\SharedLibs\Mongodb\Relations\BelongsTo;
+use Saham\SharedLibs\Mongodb\Relations\HasMany;
+
+class Wallet  Extends BaseModel
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $guarded = [];
+    protected $table = 'wallets';
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+  
+
+
+}
